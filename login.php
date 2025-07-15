51dbb391b4949e97b3b8e75111d6de4e6a42f139
Erik Vinicius <vinicius.erik.111222@gmail.com>
2025-07-15 19:16:39 -0300

    Atualiza index.html, adiciona link.txt e modifica login.php

--- login.php
+++ login.php
@@ -2,15 +2,16 @@
 session_start();
 include 'conexao.php';
 
-$usuario = $_POST['usuario'];
-$senha = $_POST['senha'];
+$usuario = $_POST['usuario'] ?? '';
+$senha = $_POST['senha'] ?? '';
 
 $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha'";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
+if ($result && $result->num_rows > 0) {
     $_SESSION['usuario'] = $usuario;
     header("Location: estoque.php");
+    exit();
 } else {
     echo "Usuário ou senha inválidos.";
 }
